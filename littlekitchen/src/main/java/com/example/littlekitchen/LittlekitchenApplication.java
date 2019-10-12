d25b668360605644ec904e485f48d86f920227b6
wangyinyan1998 <16302010074@fudan.edu.cn>
2019-10-12 21:49:10 +0800

    Add files via upload

---
+++ littlekitchen/src/main/java/com/example/littlekitchen/LittlekitchenApplication.java
@@ -0,0 +1,13 @@
+package com.example.littlekitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittlekitchenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittlekitchenApplication.class, args);
+    }
+
+}
